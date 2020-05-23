99ad9849b7c84a299545d1273c3ab4d8eaf3d49b
Matthew Winterbottom <matthewwinterbottom@outlook.com>
2020-05-23 11:49:33 +0100

    Began renaming the aspnet tables to look less autogenerated and therefore like ive done it when I havent

--- GlassProspectus.Application/Startup.cs
+++ GlassProspectus.Application/Startup.cs
@@ -1,6 +1,10 @@
+using GlassProspectus.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -8,13 +12,23 @@ namespace GlassProspectus.Application
 {
     public class Startup
     {
+        private readonly IConfiguration config;
+        public Startup(IConfiguration config)
+        {
+            this.config = config;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
-            services.AddIdentity<IdentityUser, IdentityRole>();
-            services.AddAuthorizationCore();
+            services.AddDbContext<UniDbContext>(
+
+                options => options.UseSqlServer(config.GetConnectionString("UniDbConnection"))
+
+                );
+
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<UniDbContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,7 +47,10 @@ namespace GlassProspectus.Application
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapDefaultControllerRoute();
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
             });
         }
     }
--- GlassProspectus.Repository/UniDbContext.cs
+++ GlassProspectus.Repository/UniDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace GlassProspectus.Repository
@@ -9,6 +10,18 @@ namespace GlassProspectus.Repository
             : base(options)
         {
 
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<IdentityUser>()
+                    .ToTable("Users")
+                    .Property(p => p.Id)
+                    .HasColumnName("UserId");
+
+            builder.Entity<IdentityUserRole<>>().ToTable("MyUserRoles");
         }
     }
 }
