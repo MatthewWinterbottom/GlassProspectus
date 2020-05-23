657dee6fb760e2b08568941435faa9115c3092fb
Matthew Winterbottom <matthewwinterbottom@outlook.com>
2020-05-23 10:58:32 +0100

    Add Default Servies settings and Repository Project

---
+++ GlassProspectus/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GlassProspectus.Application
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Index";
+        }
+
+        [Authorize]
+        public string Authenticate()
+        {
+            return "Authenticated";
+        }
+    }
+}
