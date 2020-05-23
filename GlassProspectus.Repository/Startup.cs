0e9557efe9d551b8b59c98868c79fb4d5d0a3436
Matthew Winterbottom <matthewwinterbottom@outlook.com>
2020-05-23 11:15:11 +0100

    Added the UniDbContext

--- GlassProspectus.Repository/Startup.cs
+++ GlassProspectus.Repository/Startup.cs
@@ -5,6 +5,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,10 +15,22 @@ namespace GlassProspectus.Repository
 {
     public class Startup
     {
+        private readonly IConfiguration config;
+        public Startup(IConfiguration config)
+        {
+            this.config = config;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<UniDbContext>(
+
+                options => options.UseSqlServer(config.GetConnectionString("UniDbConnection"))
+
+                );
+
+            services.AddIdentity<IdentityUser, IdentityRole>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ GlassProspectus.Repository/UniDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace GlassProspectus.Repository
+{
+    public class UniDbContext : IdentityDbContext
+    {
+        public UniDbContext(DbContextOptions<UniDbContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
