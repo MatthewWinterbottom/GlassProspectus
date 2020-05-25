c51091f6f97bb896ee065e34538e815b4e9739d2
Matthew Winterbottom <matthewwinterbottom@outlook.com>
2020-05-25 14:03:53 +0100

    resdsdfsdfasdf

--- GlassProspectus.Frontend/Program.cs
+++ GlassProspectus.Frontend/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace GlassProspectus.Application
+namespace GlassProspectus.Frontend
 {
     public class Program
     {
